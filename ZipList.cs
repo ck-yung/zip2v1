76f90e651f2ee6819c52aaa903a91e032def61d2
Yung, Chun Kau <yung.chun.kau@gmail.com>
2021-10-29 02:28:03 +0800

    Simple list content of a zip file

--- ZipList.cs
+++ ZipList.cs
@@ -1,10 +1,59 @@
+using ICSharpCode.SharpZipLib.Zip;
+
 namespace zip2.list
 {
     internal class Command : CommandBase
-    {        
+    {
+        string ToRatioText(long originalSize, long compressedSize)
+        {
+            if (originalSize < 1) return " 0 ";
+            if (compressedSize >= originalSize) return "-0 ";
+            compressedSize = originalSize - compressedSize;
+            compressedSize *= 100;
+            compressedSize /= originalSize;
+            if (compressedSize < 1L) return " 0 ";
+            if (compressedSize > 98L) return "99 ";
+            return string.Format("{0,2} ", compressedSize);
+        }
+
         public override int Invoke()
-        {            
-            Console.WriteLine("TODO: list.Invoke()");
+        {
+            int cntFound = 0;
+            long totalSize = 0L;
+            long totalCompressedSize = 0L;
+            // ..................... 12345678-
+            string CrcInTotalLine = "         ";
+            bool anyCrypted = false;
+            DateTime MostEarlyDate = DateTime.MaxValue;
+            DateTime MostLastDate = DateTime.MinValue;
+            using var fs = File.OpenRead(zipFilename);
+            var zipThe = new ZipFile(fs);
+            foreach (ZipEntry itm in zipThe)
+            {
+                Console.Write(ToRatioText(itm.Size, itm.CompressedSize));
+                Console.Write(string.Format("{0,12} ", itm.Size));
+                Console.Write(string.Format("{0,12} ", itm.CompressedSize));
+                Console.Write(itm.Crc.ToString("X08")+ " ");
+                Console.Write(itm.DateTime.ToString("yyyy-MM-dd HH:mm "));
+                Console.Write(itm.IsCrypted ? "*" : " ");
+                Console.WriteLine(itm.Name);
+                cntFound += 1;
+                totalSize += itm.Size;
+                totalCompressedSize = +itm.CompressedSize;
+                if (MostEarlyDate > itm.DateTime) MostEarlyDate = itm.DateTime;
+                if (MostLastDate < itm.DateTime) MostLastDate = itm.DateTime;
+                if (itm.IsCrypted) anyCrypted = true;
+            }
+            Console.Write(ToRatioText(totalSize, totalCompressedSize));
+            Console.Write(string.Format("{0,12} ", totalSize));
+            Console.Write(string.Format("{0,12} ", totalCompressedSize));
+            Console.Write(CrcInTotalLine);
+            Console.Write(MostEarlyDate.ToString("yyyy-MM-dd HH:mm "));
+            Console.Write("- ");
+            Console.Write(MostLastDate.ToString("yyyy-MM-dd HH:mm "));
+            Console.Write(anyCrypted ? "*" : " ");
+            Console.Write($"(#file={cntFound}) ");
+            Console.WriteLine(Path.GetFileName(zipFilename));
             return 0;
         }
 
